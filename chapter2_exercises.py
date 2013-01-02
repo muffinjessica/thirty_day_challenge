99e75952030fd1cece80fb02ecc03b7f6a24247b
Jessica Harris <muffinjessica@gmail.com>
2013-01-01 22:38:00 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,39 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Jessica Harris
+#Exercise 2.1
+# The numbers are in octal
+
+#Exercise 2.2
+# initially there is no output. Adding print statements displays output wehn run
+
+print 5
+x=5
+print x+1
+
+#Exercise 2.3
+#1. 8
+#2. 8.5
+#3. 4.0
+#4. 11
+#5. .....
+
+#Exercise 2.4
+#1
+pi = 3.14159
+rad = 5
+volume = (4/3.0)*pi*5**3
+print "Voulme: ", volume
+
+#2
+cp=24.95
+wtotal = (cp*.6*60) + 3+(.75*59)
+print "Wholesale cost = $",wtotal
+
+#3
+totalSec =2*(8*60+15)+3*(7*60+12)
+minutes = totalSec/60
+leftSec = totalSec - (minutes*60)
+
+minutes=minutes-8
+print "The time is 7:",minutes
